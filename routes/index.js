43772e2e0a20600e66ea2fcd1170399a70d6c0af
Luiz Alberto <luiz.trisoft@gmail.com>
2018-05-28 22:51:29 -0300

    Primeiros dados com formato json

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/list-all-broadband', function(req, res, next) {
+  res.send([
+    {"Name": "Broadband1","Price": 40.00,"Type":"bb"},			
+    
+    [
+     {"Name": "Broadband1","Price": 40.00,"Type":"bb"},
+     {"Name": "Landline","Price": 35.00,"Type":"ll", "discount": -5.00}
+    ],
+
+    [
+     {"Name": "Broadband1","Price": 40.00,"Type":"bb"},
+     {"Name": "AddonBB","Type":"addon", "additionalValue": 10.00}			
+    ],
+    [
+     {"Name": "Broadband1","Price": 40.00,"Type":"bb"},
+     {"Name": "Landline","Price": 35.00,"Type":"ll", "discount": -5.00},
+     {"Name": "AddonBB","Type":"addon", "additionalValue": 10.00}		
+    ]
+]);
+});
+
+
 module.exports = router;
