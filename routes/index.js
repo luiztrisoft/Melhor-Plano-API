5844f5b4cfaca706355a587ba7ab3e3efb6b5d94
Luiz Alberto <luiz.trisoft@gmail.com>
2018-05-28 22:56:42 -0300

    Mudança de permissões

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET home page. */
-router.get('/list-all-broadband', function(req, res, next) {
+router.get('/list-all-broadband', function (req, res, next) {
   res.send([
-    {"Name": "Broadband1","Price": 40.00,"Type":"bb"},			
-    
+    { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
     [
-     {"Name": "Broadband1","Price": 40.00,"Type":"bb"},
-     {"Name": "Landline","Price": 35.00,"Type":"ll", "discount": -5.00}
+      { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+      { "Name": "Landline", "Price": 35.00, "Type": "ll", "discount": -5.00 }
     ],
 
     [
-     {"Name": "Broadband1","Price": 40.00,"Type":"bb"},
-     {"Name": "AddonBB","Type":"addon", "additionalValue": 10.00}			
+      { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+      { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00 }
     ],
     [
-     {"Name": "Broadband1","Price": 40.00,"Type":"bb"},
-     {"Name": "Landline","Price": 35.00,"Type":"ll", "discount": -5.00},
-     {"Name": "AddonBB","Type":"addon", "additionalValue": 10.00}		
+      { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+      { "Name": "Landline", "Price": 35.00, "Type": "ll", "discount": -5.00 },
+      { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00 }
     ]
-]);
+  ]);
 });
 
 
