7c1b5cc33f86881385e10e76a68210865b76864f
Luiz Alberto <luiz.trisoft@gmail.com>
2018-05-31 00:12:01 -0300

    Json de retorno da API

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,59 @@ router.get('/', function (req, res, next) {
 
 /* Recupera dados de um json */
 router.get('/list-all-broadband', function (req, res, next) {
-  res.send([
-    { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
-    [
-      { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
-      { "Name": "Landline", "Price": 35.00, "Type": "ll", "discount": -5.00 }
-    ],
+  res.send(
+    // {
+    //   "Banda Larga 1":{"id":1, "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+    //   "Banda Larga 2":{"id":1, "Name": "Broadband2", "Price": 60.00, "Type": "bb" }       
+    // }
 
-    [
-      { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
-      { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00 }
-    ],
-    [
-      { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
-      { "Name": "Landline", "Price": 35.00, "Type": "ll", "discount": -5.00 },
-      { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00 }
-    ]
-  ]);
+   [ 
+     {
+      "nome":"Banda larga 1",
+      "pacote":[
+        {"id":1, "Name": "Broadband1", "Price": 40.00, "Type": "bb" }        
+      ],      
+      "total":40.00
+    },
+
+    {
+      "nome":"Banda larga 2",
+      "pacote":[
+        {"id":2, "Name": "Broadband2", "Price": 60.00, "Type": "bb" }        
+      ],
+      "total":60.00      
+    },
+
+    {
+      "nome" : "Banda Larga + TV",
+      "pacote":[
+        {"id":1, "Name": "Broadband2", "Price": 60.00, "Type": "bb" },
+        {"id":3, "Name": "TV1", "Price": 50.00, "Type": "tv" },
+        { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00, "Price":0.00 }  
+      ],
+      "total":90.00// ainda sem desconto
+    }
+  ]
+    
+  
+  //   [
+  //   { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+  //   [
+  //     { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+  //     { "Name": "Landline", "Price": 35.00, "Type": "ll", "discount": -5.00 }
+  //   ],
+
+  //   [
+  //     { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+  //     { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00 }
+  //   ],
+  //   [
+  //     { "Name": "Broadband1", "Price": 40.00, "Type": "bb" },
+  //     { "Name": "Landline", "Price": 35.00, "Type": "ll", "discount": -5.00 },
+  //     { "Name": "AddonBB", "Type": "addon", "additionalValue": 10.00 }
+  //   ]
+  // ]
+);
 });
 
 
