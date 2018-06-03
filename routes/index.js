57228ae16d60ced2eff68533df238a3e71f656a5
Luiz Alberto <luiz.trisoft@gmail.com>
2018-06-03 01:42:25 -0300

    Correções a serem feitas

--- routes/index.js
+++ routes/index.js
@@ -35,15 +35,17 @@ var planos = [
   { "id": 9, "name": "AddonTV-Ex2", "price": 0.00, "type": "addon", "additionalPrice": 5.00 }
 ];
 
-function getPlano1() { return planos[0] }
-function getPlano2() { return planos[1] }
-function getPlano3() { return planos[2] }
-function getPlano4() { return planos[3] }
-function getPlano5() { return planos[4] }
-function getPlano6() { return planos[5] }
-function getPlano7() { return planos[6] }
-function getPlano8() { return planos[7] }
-function getPlano9() { return JSON.stringify(planos[8]) }
+// function getPlano1() { return planos[0] }
+// function getPlano2() { return planos[1] }
+// function getPlano3() { return planos[2] }
+// function getPlano4() { return planos[3] }
+// function getPlano5() { return planos[4] }
+// function getPlano6() { return planos[5] }
+// function getPlano7() { return planos[6] }
+// function getPlano8() { return planos[7] }
+// function getPlano9() { return JSON.stringify(planos[8]) }
+
+var possibilidades = [];
 
 function montarCombos() {
   //definindo nós iniciais
@@ -53,53 +55,87 @@ function montarCombos() {
       broadbands.push(planos[i]);
     }
   }
-  // for(var i = 0; i < broadbands.length; ++i){
-  // }
-  return criarCombinacoes(broadbands[0]);
+  possibilidades = [];
+
+  for (var i = 0; i < broadbands.length; ++i) {
+    criarCombinacoes(broadbands[i]);
+  }
+  return possibilidades;
 }
+
+// function criarCombinacoes(broadbandInicial) {
+//   var arrayDeCombinacoes = gerarCombinacoesPossiveis(broadbandInicial);
+//   return arrayDeCombinacoes;
+// }
 
 function criarCombinacoes(broadbandInicial) {
-  var arrayDeCombinacoes = gerarCombinacoesPossiveis(broadbandInicial);
-  return arrayDeCombinacoes;
-}
-
-function gerarCombinacoesPossiveis(broadbandInicial) {  
   if (broadbandInicial.id === 1) {
-    return definirRegrasBB1()    
+    definirRegrasBB1()
   } else if (broadbandInicial.id === 2) {
-    //console.log(2);
+    definirRegrasBB2()
   } else {
     return;
   }
 }
 
-function definirRegrasBB1(){
-  var possibilidades = [];
-  planos[4].additionalPrice = -5;//paliativo
+function definirRegrasBB1() {
+  planos[4].additionalPrice = -5;
   var i, j, k, x = [planos[0], planos[4], planos[5]];
-  possibilidades.push(criarPlanoJSON([planos[0]]));//primeiro plano
+  possibilidades.push(criarPlanoJSON([planos[0]]));
+  
   for (i = 0; i < x.length; ++i) {
     if (x[i] === x[0]) {
       for (j = i + 1; j < x.length; ++j) {
-        // possibilidades.push([  x[i], x[j] ]);//No push é onde monta o json     
         possibilidades.push(criarPlanoJSON([x[i], x[j]]));
         for (k = j + 1; k < x.length; ++k) {
-          // possibilidades.push([ x[i], x[j], x[k]]);
           possibilidades.push(criarPlanoJSON([x[i], x[j], x[k]]));
         }
       }
     }
   }
-  return possibilidades;
 }
 
-function definirRegrasBB2(){
-  
+function definirRegrasBB2() {
+  planos[4].additionalPrice = -10;
+  var x = [planos[1], planos[2], planos[3], planos[4], planos[5]]//, planos[6], planos[7], planos[8]];
+  var tv1 = false;
+  var tv2 = false;
+  possibilidades.push(criarPlanoJSON([planos[1]]));
+
+  for (i = 0; i < x.length; ++i) {
+    if (x[i] === x[0]) {
+      for (j = i + 1; j < x.length; ++j) {
+        possibilidades.push(criarPlanoJSON([x[i], x[j]]));
+        if (x[j].id === 3) {
+          tv1 = true;
+        }
+        for (k = j + 1; k < x.length; ++k) {
+          if (tv1 == true && x[k].id === 4) {            
+            console.log('err 1')
+            tv2 = true;            
+            console.log('Se plano 3 ja existe nao inserimos o plano 4 \n Não há necessidade de tratar o inverso')
+          } else {
+            possibilidades.push(criarPlanoJSON([x[i], x[j], x[k]]));
+          }
+          for (l = k + 1; l < x.length; ++l) {
+            if (tv1 == true && x[k].id === 4) {
+              console.log('err 2')
+              tv2 = true;
+              //TODO tratar a linha fixa que tambem pode ter adicional de -30
+              //x[k].additionalPrice = -30
+            } else {              
+              possibilidades.push(criarPlanoJSON([x[i], x[j], x[k], x[l]]));
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 function criarPlanoJSON(plano) {
   var p = new Object();
-  p.descricao = "Plano ";
+  p.descricao = "PLANO ";
   p.pacote = plano;
   var total = 0;
   for (var i = 0; i < plano.length; ++i) {
@@ -108,6 +144,5 @@ function criarPlanoJSON(plano) {
   p.total = total;
   return p;
 }
-
 
 module.exports = router;
